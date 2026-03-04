6100f721d17bbe45b8d5a5a92e37b6fb1b3464e5
Solareon <769465+solareon@users.noreply.github.com>
2026-03-04 21:02:10 +0000

    feat(props): extract props to modular function

---
+++ client/props.lua
@@ -0,0 +1,52 @@
+local playerState = LocalPlayer.state
+
+AddStateBagChangeHandler('red40_mining', ('player:%s'):format(cache.serverId), function(_, _, value)
+    lib.print.info('State bag changed', value)
+    if not value or type(value) ~= 'table' then return end
+
+    if not value.activity then return end
+
+    if value.entity then
+        while not NetworkDoesEntityExistWithNetworkId(value.entity) do
+            Wait(35)
+        end
+
+        local entity = NetworkGetEntityFromNetworkId(value.entity)
+
+        if entity == 0 or not DoesEntityExist(entity) then return end
+
+        if NetworkGetEntityOwner(entity) ~= cache.playerId then
+            while NetworkGetEntityOwner(entity) ~= cache.playerId do
+                NetworkRequestControlOfEntity(entity)
+                Wait(35)
+            end
+        end
+
+        if value.bone then
+            local offset, rotation = value.offset, value.rotation
+
+            AttachEntityToEntity(entity, cache.ped, GetPedBoneIndex(cache.ped, value.bone), offset.x, offset.y, offset.z,
+                rotation.x, rotation.y, rotation.z, true, true, false, false, 1,
+                true)
+        end
+    end
+end)
+
+
+lib.onCache('vehicle', function(vehicle)
+    if playerState.red40_mining and vehicle then
+        TriggerServerEvent('red40_mining:server:stopMining')
+    end
+end)
+
+lib.onCache('ped', function(ped)
+    if playerState.red40_mining and ped then
+        TriggerServerEvent('red40_mining:server:stopMining')
+    end
+end)
+
+lib.onCache('weapon', function(weapon)
+    if playerState.red40_mining and weapon then
+        TriggerServerEvent('red40_mining:server:stopMining')
+    end
+end)
---
+++ server/props.lua
@@ -0,0 +1,54 @@
+local config = require 'config.shared'
+
+MiningObjects = {}
+MiningTools = {}
+
+function DeleteMiningObject(source)
+    local object = MiningObjects[source]
+
+    if object and DoesEntityExist(object) then
+        DeleteEntity(object)
+        Player(source).state:set('red40_mining', nil, true)
+        MiningObjects[source] = nil
+    end
+end
+
+function StartMining(src, activity, tool)
+    if MiningObjects[src] then
+        DeleteEntity(MiningObjects[src])
+        MiningObjects[src] = nil
+    end
+
+    local ped = GetPlayerPed(src)
+
+    if GetVehiclePedIsIn(ped, false) > 0 then
+        return Notify(src, locale('vehicle_mining'), 'error')
+    end
+
+    local coords = GetEntityCoords(ped)
+
+    local object = CreateObject(config[activity].tools[tool].prop, coords.x, coords.y, coords.z - 20, true, true, true)
+
+    while not DoesEntityExist(object) do
+        Wait(50)
+    end
+
+    SetEntityIgnoreRequestControlFilter(object, true)
+
+    Player(src).state:set('red40_mining', {
+        entity = NetworkGetNetworkIdFromEntity(object),
+        tool = tool,
+        bone = config[activity].tools[tool].bone,
+        offset = config[activity].tools[tool].offset,
+        rotation = config[activity].tools[tool].rotation,
+        activity = activity,
+    }, true)
+
+    MiningObjects[src] = object
+    MiningTools[src] = tool
+end
+
+RegisterNetEvent('red40_mining:server:stopMining', function()
+    local src = source
+    DeleteMiningObject(src)
+end)
